b78ebca1d188491a1991b85a4b1a2b4cccf4a370
John Hunter <johnroberthunter1@gmail.com>
2026-05-23 12:36:40 -0600

    time person connedts stuff

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const fs = require("fs");
 const PORT = 3000;
 let userId = 1;
 const clients = [];
+
+function logging(message) {
+  const time = new Date().toISOString();
+  const logMessage = `[${time}] ${message}\n`;
+
+  fs.appendFile("chat.log", logMessage, (err) => {
+    if (err) console.error("Failed to write to chat.log:", err);
+  });
+}
