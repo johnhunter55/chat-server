49f2f53817daa6311341d7348396bc7290dec4c3
John Hunter <johnroberthunter1@gmail.com>
2026-05-23 12:40:37 -0600

    sending messages

--- server.js
+++ server.js
@@ -13,3 +13,38 @@ function logging(message) {
     if (err) console.error("Failed to write to chat.log:", err);
   });
 }
+
+function broadcast(message, senderSocket) {
+  clients.forEach((client) => {
+    if (client.socket !== senderSocket) {
+      client.socket.write(message);
+    }
+  });
+}
+
+const server = net.createServer((socket) => {
+  const clientId = `Client${userId++}`;
+  clients.push({ id: clientId, socket });
+
+  socket.write("Welcome to the server! :D\n");
+
+  broadcast(`${clientId} has connected dont bully.\n`, socket);
+
+  logToFile(`${clientId} connected.`);
+
+  socket.on("data", (data) => {
+    const message = data.toString().trim();
+    if (message) {
+      broadcast(`${clientId}: ${message}\n`, socket);
+
+      logToFile(`${clientId}: ${message}`);
+    }
+  });
+  socket.on("end", () => {
+    handleDisconnect(socket, clientId);
+  });
+
+  socket.on("error", () => {
+    handleDisconnect(socket, clientId);
+  });
+});
